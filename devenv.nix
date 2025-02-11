c3ae84e943ddf8a479fa7c9cdd839338ace87165
orpheus <orpheus@computerdream.club>
2025-02-11 15:25:10 -0800

    A few more devenv improvements
    - Try to work towards packaging...

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,9 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git pkgs.gprbuild pkgs.gnat ];
+  packages = [
+    pkgs.git pkgs.gnumake pkgs.gprbuild pkgs.bash pkgs.gnat
+  ] ++ lib.optionals (!config.container.isBuilding) [ ];
 
   # https://devenv.sh/languages/
   languages.c.enable = true;
@@ -23,27 +25,45 @@
   # '';
 
   scripts.words.exec = ''
+     cd "$DEVENV_ROOT";
     ./bin/words "$@";
   '';
 
   enterShell = ''
-    git --version
+    mkdir -p ~/.local/bin
+
+    cat <<EOF > ~/.local/bin/whitakers-words
+    #!/usr/bin/env sh
+    cd $DEVENV_ROOT
+    exec ./bin/words "\$@"
+    EOF
+
+    chmod +x ~/.local/bin/whitakers-words
   '';
 
   # https://devenv.sh/tasks/
   tasks = {
-    "words:install".exec = "make";
-    "devenv:enterShell".after = [ "words:install" ];
+    "words:install".exec = "devenv shell make";
+    # "devenv:enterShell".after = [ "words:install" ];
   };
 
   # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
+  # outputs = {
+  #   myproject.myapp = import ./myapp { inherit pkgs; };
+  #   # git = pkgs.git;
+  # };
+
+  # processes.ww.exec = "/bin/words";
+
+  # containers."ww".name = "whitakers-words";
+  # containers."ww".startupCommand = config.processes.ww.exec;
 }
